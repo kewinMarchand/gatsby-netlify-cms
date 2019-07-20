7d7c2d86e96bea5a01fd7660375978083c926642
kewinMarchand <k.marchand@purjus.fr>
2019-07-21 00:48:40 +0200

    menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Kaldi" style={{ width: 'auto' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -60,7 +60,6 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <a href="/admin" target="_blank">Admin</a>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
@@ -76,11 +75,19 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
+              <a 
+                className="navbar-item" 
+                href="/admin" 
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Admin
+              </a>
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/kewinMarchand/gatsby-netlify-cms"
                 target="_blank"
                 rel="noopener noreferrer"
               >
